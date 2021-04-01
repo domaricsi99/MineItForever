078980c2aa6391f6c6e0b08ae161d2fae6157dc9
domaricsi <domaricsi99@gmail.com>
2021-04-01 12:03:20 +0200

    Create all dll

---
+++ OENIK_PROG4_2021_1_IHBTEL_P9KQFH/Model/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Character
+    {
+        public int MyProperty { get; set; }
+    }
+}
