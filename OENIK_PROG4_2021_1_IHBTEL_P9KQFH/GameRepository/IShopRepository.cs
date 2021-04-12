20d474d18f8a45a3b288386479252dc72636a0c4
domaricsi <domaricsi99@gmail.com>
2021-04-12 20:39:03 +0200

    Create pickaxes

---
+++ OENIK_PROG4_2021_1_IHBTEL_P9KQFH/GameRepository/IShopRepository.cs
@@ -0,0 +1,14 @@
+﻿using GameModelDll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRepository
+{
+    interface IShopRepository
+    {
+        public List<Pickax> PickaxList();
+    }
+}
---
+++ OENIK_PROG4_2021_1_IHBTEL_P9KQFH/GameRepository/ShopRepsitory.cs
@@ -0,0 +1,54 @@
+﻿using GameModelDll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRepository
+{
+    class ShopRepsitory : IShopRepository
+    {
+        public List<Pickax> PickaxList()
+        {
+            List<Pickax> pickaxes = new List<Pickax>();
+
+            pickaxes.Add(new Pickax()
+            {
+                Name = "basic",
+                Price = 0,
+                Strength = 0,
+            });
+
+            pickaxes.Add(new Pickax()
+            {
+                Name = "ketto",
+                Price = 10,
+                Strength = 1,
+            });
+
+            pickaxes.Add(new Pickax()
+            {
+                Name = "intermediate",
+                Price = 20,
+                Strength = 2,
+            });
+
+            pickaxes.Add(new Pickax()
+            {
+                Name = "advanced",
+                Price = 30,
+                Strength = 3,
+            });
+
+            pickaxes.Add(new Pickax()
+            {
+                Name = "professional",
+                Price = 40,
+                Strength = 4,
+            });
+
+            return pickaxes;
+        }
+    }
+}
