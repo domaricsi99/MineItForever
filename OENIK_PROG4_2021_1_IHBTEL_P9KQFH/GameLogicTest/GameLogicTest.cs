a497c9cf55034364c2a0060a13685f4a9b496fd6
domaricsi <domaricsi99@gmail.com>
2021-04-28 22:01:07 +0200

    Start write the tests

--- OENIK_PROG4_2021_1_IHBTEL_P9KQFH/GameLogicTest/GameLogicTest.cs
+++ OENIK_PROG4_2021_1_IHBTEL_P9KQFH/GameLogicTest/GameLogicTest.cs
@@ -1,42 +1,55 @@
-﻿//// <copyright file="GameLogicTest.cs" company="PlaceholderCompany">
-//// Copyright (c) PlaceholderCompany. All rights reserved.
-//// </copyright>
+﻿// <copyright file="GameLogicTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-//namespace GameLogicTest
-//{
-//    using GameLogicDll;
-//    using GameModelDll;
-//    using GameRepository;
-//    using Moq;
-//    using NUnit.Framework;
+namespace GameLogicTest
+{
+    using GameLogicDll;
+    using GameModelDll;
+    using GameRepository;
+    using Moq;
+    using NUnit.Framework;
 
-//    /// <summary>
-//    /// Game logic test class.
-//    /// </summary>
-//    [TestFixture]
-//    class GameLogicTest
-//    {
-//        GameLogic logic;
-//        MapRepository mapRepo;
-//        CharacterRepository charRepo;
-//        GameModel model;
+    /// <summary>
+    /// Game logic test class.
+    /// </summary>
+    [TestFixture]
+    class GameLogicTest
+    {
+        GameLogic logic;
+        MapRepository mapRepo;
+        CharacterRepository charRepo;
+        GameModel model;
+        Ore[,] matrix;
 
-//        /// <summary>
-//        /// Init mock logic.
-//        /// </summary>
-//        [SetUp]
-//        public void Init()
-//        {
-//            this.charRepo = new CharacterRepository();
-//            this.mapRepo = new MapRepository();
-//            this.logic = new GameLogic(this.mapRepo, this.charRepo);
-//            this.model = new GameModel();
-//        }
+        /// <summary>
+        /// Init mock logic.
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            this.charRepo = new CharacterRepository();
+            this.mapRepo = new MapRepository();
+            this.logic = new GameLogic(this.mapRepo, this.charRepo);
+            this.model = new GameModel();
+            this.matrix = new Ore[5, 5];
+        }
 
-//        [Test]
-//        public void CanJumpMethod()
-//        {
+        [Test]
+        public void Movement()
+        {
+            Direction direction = Direction.Left;
+            var value = this.logic.Movement(direction);
 
-//        }
-//    }
-//}
+            Assert.AreEqual(true, value);
+        }
+
+        [Test]
+        public void JumpWhileFalling()
+        {
+            bool value = this.logic.CanJumpMethod();
+
+            Assert.AreEqual(true, value);
+        }
+    }
+}
